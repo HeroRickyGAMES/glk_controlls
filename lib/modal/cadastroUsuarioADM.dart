a35db8b9d489333a2a4b10059fbf1233dda9accb
herorickygames <ricojn9@gmail.com>
2023-02-22 17:03:12 -0300

    Adição do SingleChildScrollView no layout

--- lib/modal/cadastroUsuarioADM.dart
+++ lib/modal/cadastroUsuarioADM.dart
@@ -18,121 +18,123 @@ class _cadastroUsuarioModalState extends State<cadastroUsuarioModal> {
       appBar: AppBar(
         title: Text("Cadastro Users")
       ),
-    body: Column(
-      children: [
-        Container(
-          padding: EdgeInsets.all(16),
-          child: RadioListTile(
-            title: Text(
-              "Sou um Operador",
-              style: TextStyle(
-                  fontSize: 20
+    body: SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            child: RadioListTile(
+              title: Text(
+                "Sou um Operador",
+                style: TextStyle(
+                    fontSize: 20
+                ),
               ),
+              value: "porteiro",
+              groupValue: tipos,
+              onChanged: (value){
+                setState(() {
+                  tipos = value.toString();
+                  cnpjourg = 'Seu RG';
+                  NomeOuRazao = 'Seu Nome completo';
+                });
+              },
             ),
-            value: "porteiro",
-            groupValue: tipos,
-            onChanged: (value){
-              setState(() {
-                tipos = value.toString();
-                cnpjourg = 'Seu RG';
-                NomeOuRazao = 'Seu Nome completo';
-              });
-            },
           ),
-        ),
-        Container(
-          padding: EdgeInsets.all(16),
-          child: RadioListTile(
-            title: Text(
-              "Sou uma Empresa",
-              style: TextStyle(
-                  fontSize: 20
+          Container(
+            padding: EdgeInsets.all(16),
+            child: RadioListTile(
+              title: Text(
+                "Sou uma Empresa",
+                style: TextStyle(
+                    fontSize: 20
+                ),
               ),
-            ),
-            value: "empresa",
-            groupValue: tipos,
-            onChanged: (value){
-              setState(() {
-                tipos = value.toString();
-                cnpjourg = 'CNPJ da Empresa';
-                NomeOuRazao = 'Razão Social';
-              });
-            },
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.all(16),
-          child: TextFormField(
-            onChanged: (value){
-
-            },
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            hintText: 'Nome Completo',
-            hintStyle: TextStyle(
-                fontSize: 20
+              value: "empresa",
+              groupValue: tipos,
+              onChanged: (value){
+                setState(() {
+                  tipos = value.toString();
+                  cnpjourg = 'CNPJ da Empresa';
+                  NomeOuRazao = 'Razão Social';
+                });
+              },
             ),
           ),
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.all(16),
-          child: TextFormField(
-            onChanged: (value){
+          Container(
+            padding: EdgeInsets.all(16),
+            child: TextFormField(
+              onChanged: (value){
 
-            },
+              },
             decoration: InputDecoration(
               border: OutlineInputBorder(),
-              hintText: 'RG',
+              hintText: 'Nome Completo',
               hintStyle: TextStyle(
                   fontSize: 20
               ),
             ),
+            ),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.all(16),
-          child: TextFormField(
-            onChanged: (value){
+          Container(
+            padding: EdgeInsets.all(16),
+            child: TextFormField(
+              onChanged: (value){
 
-            },
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Email',
-              hintStyle: TextStyle(
-                  fontSize: 20
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'RG',
+                hintStyle: TextStyle(
+                    fontSize: 20
+                ),
               ),
             ),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.all(16),
-          child: TextFormField(
-            onChanged: (value){
+          Container(
+            padding: EdgeInsets.all(16),
+            child: TextFormField(
+              onChanged: (value){
 
-            },
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Senha',
-              hintStyle: TextStyle(
-                  fontSize: 20
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Email',
+                hintStyle: TextStyle(
+                    fontSize: 20
+                ),
               ),
             ),
           ),
-        ),
-        Container(
-          padding: EdgeInsets.all(16),
-          child: ElevatedButton(
-              onPressed: (){
+          Container(
+            padding: EdgeInsets.all(16),
+            child: TextFormField(
+              onChanged: (value){
 
-          },
-              child:
-              Text(
-                  'Cadastrar usuario'
-              )
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Senha',
+                hintStyle: TextStyle(
+                    fontSize: 20
+                ),
+              ),
+            ),
           ),
-        )
-      ],
+          Container(
+            padding: EdgeInsets.all(16),
+            child: ElevatedButton(
+                onPressed: (){
+
+            },
+                child:
+                Text(
+                    'Cadastrar usuario'
+                )
+            ),
+          )
+        ],
+      ),
     ),
     );
   }
