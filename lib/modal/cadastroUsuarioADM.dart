f44e1f11d6cdc71433af1dbec581d63c90c7d19e
HeroRickyGAMES <ricojn9@gmail.com>
2023-02-22 11:37:25 -0800

    Adição da interface do formulario de cadastro de usuario

--- lib/modal/cadastroUsuarioADM.dart
+++ lib/modal/cadastroUsuarioADM.dart
@@ -8,8 +8,132 @@ class cadastroUsuarioModal extends StatefulWidget {
 }
 
 class _cadastroUsuarioModalState extends State<cadastroUsuarioModal> {
+  String? tipos;
+  String? cnpjourg = 'Seu RG';
+  String? NomeOuRazao = 'Seu Nome completo';
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cadastro Users")
+      ),
+    body: Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(16),
+          child: RadioListTile(
+            title: Text(
+              "Sou um Operador",
+              style: TextStyle(
+                  fontSize: 20
+              ),
+            ),
+            value: "porteiro",
+            groupValue: tipos,
+            onChanged: (value){
+              setState(() {
+                tipos = value.toString();
+                cnpjourg = 'Seu RG';
+                NomeOuRazao = 'Seu Nome completo';
+              });
+            },
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(16),
+          child: RadioListTile(
+            title: Text(
+              "Sou uma Empresa",
+              style: TextStyle(
+                  fontSize: 20
+              ),
+            ),
+            value: "empresa",
+            groupValue: tipos,
+            onChanged: (value){
+              setState(() {
+                tipos = value.toString();
+                cnpjourg = 'CNPJ da Empresa';
+                NomeOuRazao = 'Razão Social';
+              });
+            },
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(16),
+          child: TextFormField(
+            onChanged: (value){
+
+            },
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Nome Completo',
+            hintStyle: TextStyle(
+                fontSize: 20
+            ),
+          ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(16),
+          child: TextFormField(
+            onChanged: (value){
+
+            },
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'RG',
+              hintStyle: TextStyle(
+                  fontSize: 20
+              ),
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(16),
+          child: TextFormField(
+            onChanged: (value){
+
+            },
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Email',
+              hintStyle: TextStyle(
+                  fontSize: 20
+              ),
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(16),
+          child: TextFormField(
+            onChanged: (value){
+
+            },
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Senha',
+              hintStyle: TextStyle(
+                  fontSize: 20
+              ),
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(16),
+          child: ElevatedButton(
+              onPressed: (){
+
+          },
+              child:
+              Text(
+                  'Cadastrar usuario'
+              )
+          ),
+        )
+      ],
+    ),
+    );
   }
 }
