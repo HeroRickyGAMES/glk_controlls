8b4bcfcf85684efe2790532e8848b8715009e404
herorickygames <ricojn9@gmail.com>
2023-02-24 11:38:28 -0300

    dição do singlechildscrollview no mainempresa

--- lib/mainEmpresa.dart
+++ lib/mainEmpresa.dart
@@ -125,162 +125,164 @@ class _mainEmpresaState extends State<mainEmpresa> {
           Container(
             padding: EdgeInsets.all(16),
             child:
-            Column(
-              children: [
-                Text(
-                  'Pesquisar Placa:',
-                  style: TextStyle(
-                      fontSize: 20
-                  ),
-                ),
-                TextFormField(
-                  onChanged: (valor){
-
-                    String value = valor.replaceAll(' ', '').toUpperCase();;
-
-                    holderPlaca = value.replaceAllMapped(
-                      RegExp(r'^([a-zA-Z]{3})([0-9]{4})$'),
-                          (Match m) => '${m[1]}-${m[2]}',
-                    );
-                  },
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Colors.green
-                        )
-                    ),
-                    hintStyle: TextStyle(
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Text(
+                    'Pesquisar Placa:',
+                    style: TextStyle(
                         fontSize: 20
                     ),
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(16),
-                  child: ElevatedButton(
-                    onPressed: (){
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context){
-                            return pesquisa(holderPlaca);
-                          }));
-                  },
-                    child: Text(
-                        'Pesquisar'
+                  TextFormField(
+                    onChanged: (valor){
+
+                      String value = valor.replaceAll(' ', '').toUpperCase();;
+
+                      holderPlaca = value.replaceAllMapped(
+                        RegExp(r'^([a-zA-Z]{3})([0-9]{4})$'),
+                            (Match m) => '${m[1]}-${m[2]}',
+                      );
+                    },
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors.green
+                          )
+                      ),
+                      hintStyle: TextStyle(
+                          fontSize: 20
+                      ),
                     ),
                   ),
-                ),
-                StreamBuilder(
-                    stream: FirebaseFirestore
-                        .instance
-                        .collection('Autorizacoes')
-                        .snapshots(),
-                    builder: (BuildContext context,
-                        AsyncSnapshot<QuerySnapshot> snapshot) {
-                      if (!snapshot.hasData) {
-                        return Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                      return Container(
-                        height: 900,
-                        width: double.infinity,
-                        child: GridView.count(
-                          padding: const EdgeInsets.all(5),
-                          crossAxisSpacing: 5,
-                          mainAxisSpacing: 5,
-                          crossAxisCount: 2,
-                          childAspectRatio: 0.57,
-                          children:
-                          snapshot.data!.docs.map((documents) {
-                            String lacre = '${documents['LacreouNao']}';
-                            String ColetaOuEntrega = '${documents['ColetaOuEntrega']}';
-                            bool lacrebool = false;
-                            bool coletaBool = false;
-                            String lacrado = '';
-                            String ColetaOuEntregast = '';
-                            idDocumento = documents.id;
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    child: ElevatedButton(
+                      onPressed: (){
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context){
+                              return pesquisa(holderPlaca);
+                            }));
+                    },
+                      child: Text(
+                          'Pesquisar'
+                      ),
+                    ),
+                  ),
+                  StreamBuilder(
+                      stream: FirebaseFirestore
+                          .instance
+                          .collection('Autorizacoes')
+                          .snapshots(),
+                      builder: (BuildContext context,
+                          AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (!snapshot.hasData) {
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        return Container(
+                          height: 900,
+                          width: double.infinity,
+                          child: GridView.count(
+                            padding: const EdgeInsets.all(5),
+                            crossAxisSpacing: 5,
+                            mainAxisSpacing: 5,
+                            crossAxisCount: 2,
+                            childAspectRatio: 0.57,
+                            children:
+                            snapshot.data!.docs.map((documents) {
+                              String lacre = '${documents['LacreouNao']}';
+                              String ColetaOuEntrega = '${documents['ColetaOuEntrega']}';
+                              bool lacrebool = false;
+                              bool coletaBool = false;
+                              String lacrado = '';
+                              String ColetaOuEntregast = '';
+                              idDocumento = documents.id;
 
-                              if(documents['Status'] != 'Autorizado a entrar na empresa'){
-                                if(lacre == 'lacre'){
-                                  lacrebool = true;
-                                  lacrado = 'Lacrado';
-                                }
-                                if(lacre == 'naolacrado'){
-                                  lacrebool = false;
-                                  lacrado = 'Não Lacrado';
-                                }
-                                if(ColetaOuEntrega == 'coleta'){
-                                  coletaBool = true;
-                                  ColetaOuEntregast = 'Coleta';
-                                }
-                                if(ColetaOuEntrega == 'entrega'){
-                                  coletaBool = false;
-                                  ColetaOuEntregast = 'Entrega';
-                                }
-                                return Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Container(
-                                    color: Colors.grey[300],
-                                    padding: EdgeInsets.all(16),
-                                    child:
-                                    Column(
-                                      children: [
-                                        Text(
-                                          documents['PlacaVeiculo'],
-                                          style:
-                                          TextStyle(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.bold
-                                          ),
-                                        ),
-                                        Container(
-                                          padding: EdgeInsets.all(16),
-                                          child: Image
-                                              .network(
-                                            documents['uriImage'],
-                                            fit: BoxFit.cover,
-                                          ),
-                                        ),
-                                        Container(
-                                          padding: EdgeInsets.all(16),
-                                          child: Text(
-                                            'Status: \n' +
-                                                documents['Status'],
-                                            style: TextStyle(
+                                if(documents['Status'] != 'Autorizado a entrar na empresa'){
+                                  if(lacre == 'lacre'){
+                                    lacrebool = true;
+                                    lacrado = 'Lacrado';
+                                  }
+                                  if(lacre == 'naolacrado'){
+                                    lacrebool = false;
+                                    lacrado = 'Não Lacrado';
+                                  }
+                                  if(ColetaOuEntrega == 'coleta'){
+                                    coletaBool = true;
+                                    ColetaOuEntregast = 'Coleta';
+                                  }
+                                  if(ColetaOuEntrega == 'entrega'){
+                                    coletaBool = false;
+                                    ColetaOuEntregast = 'Entrega';
+                                  }
+                                  return Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(
+                                      color: Colors.grey[300],
+                                      padding: EdgeInsets.all(16),
+                                      child:
+                                      Column(
+                                        children: [
+                                          Text(
+                                            documents['PlacaVeiculo'],
+                                            style:
+                                            TextStyle(
                                                 fontSize: 20,
                                                 fontWeight: FontWeight.bold
                                             ),
                                           ),
-                                        ),
-                                        ElevatedButton(
-                                            onPressed: (){
-
-                                            },
+                                          Container(
+                                            padding: EdgeInsets.all(16),
+                                            child: Image
+                                                .network(
+                                              documents['uriImage'],
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                          Container(
+                                            padding: EdgeInsets.all(16),
                                             child: Text(
-                                              'Editar',
+                                              'Status: \n' +
+                                                  documents['Status'],
                                               style: TextStyle(
                                                   fontSize: 20,
                                                   fontWeight: FontWeight.bold
                                               ),
-                                            )
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                );
-                              }else{
-                                return Text('');
-                              }
-                            }
-                          ).toList(),
-                        ),
-                      );
-                    }
-                ),
+                                            ),
+                                          ),
+                                          ElevatedButton(
+                                              onPressed: (){
 
-              ],
+                                              },
+                                              child: Text(
+                                                'Editar',
+                                                style: TextStyle(
+                                                    fontSize: 20,
+                                                    fontWeight: FontWeight.bold
+                                                ),
+                                              )
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                }else{
+                                  return Text('');
+                                }
+                              }
+                            ).toList(),
+                          ),
+                        );
+                      }
+                  ),
+
+                ],
+              ),
             ),
           )
         ],
