65027465b017bad757d23442ca6f80ca534c9609
herorickygames <ricojn9@gmail.com>
2023-03-07 14:20:02 -0300

    Adição do RelatorioDays no cadastro de empresas

--- lib/modal/cadastroEmpresa.dart
+++ lib/modal/cadastroEmpresa.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
+List<String> tags = [];
 class cadastroEmpresa extends StatefulWidget {
   const cadastroEmpresa({Key? key}) : super(key: key);
 
@@ -14,6 +15,27 @@ class cadastroEmpresa extends StatefulWidget {
 class _cadastroEmpresaState extends State<cadastroEmpresa> {
   @override
   Widget build(BuildContext context) {
+
+    final _textController = TextEditingController();
+    final respNameController = TextEditingController();
+    final telNumController = TextEditingController();
+
+    print(tags);
+
+    void _addTag(String tag) {
+      if (tag.isNotEmpty) {
+        setState(() {
+          tags.add(tag);
+        });
+        _textController.clear();
+      }
+    }
+
+    void _removeTag(String tag) {
+      setState(() {
+        tags.remove(tag);
+      });
+    }
 
     String empresaName = '';
     String galpaoNum = '';
@@ -35,6 +57,45 @@ class _cadastroEmpresaState extends State<cadastroEmpresa> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Text(
+                  'Preencha o campo de Dias primeiro!\n(Aperte Enter quando adicionar um dia)',
+                style: TextStyle(
+                  fontSize: 20
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Wrap(
+                      spacing: 8.0,
+                      runSpacing: 4.0,
+                      children: tags.map((tag) {
+                        return InputChip(
+                          label: Text(tag),
+                          onDeleted: () => _removeTag(tag),
+                        );
+                      }).toList(),
+                    ),
+                    TextFormField(
+                      controller: _textController,
+                      keyboardType: TextInputType.number,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Dias em que a empresa pode usar o relatorio',
+                        hintStyle: TextStyle(
+                            fontSize: 20
+                        ),
+                      ),
+                      onFieldSubmitted: _addTag,
+                    ),
+                  ],
+                ),
+              ),
               Container(
                 padding: EdgeInsets.all(16),
                 child: TextFormField(
@@ -79,6 +140,14 @@ class _cadastroEmpresaState extends State<cadastroEmpresa> {
                   onPressed: (){
                     int number = int.parse(galpaoNum);
 
+                    Fluttertoast.showToast(
+                      msg: 'Calculando.',
+                      toastLength: Toast.LENGTH_SHORT,
+                      timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.black,
+                      textColor: Colors.white,
+                      fontSize: 20,
+                    );
                     for (int i = number; i >= 1; i--) {
                       print(i);
                       lista.add('G-0${i} ' + empresaName);
@@ -97,8 +166,9 @@ class _cadastroEmpresaState extends State<cadastroEmpresa> {
               Container(
                 padding: EdgeInsets.all(16),
                 child: TextFormField(
+                  controller: respNameController,
                   onChanged: (valor){
-                    respName = valor;
+                    respName = valor.toUpperCase();
                     //Mudou mandou para a String
                   },
                   keyboardType: TextInputType.name,
@@ -116,8 +186,16 @@ class _cadastroEmpresaState extends State<cadastroEmpresa> {
               Container(
                 padding: EdgeInsets.all(16),
                 child: TextFormField(
+                  controller: telNumController,
                   onChanged: (valor){
-                    telNum = valor;
+                    respNameController.text = respName;
+
+
+                    String valorpuro = valor.toUpperCase();
+                    telNum = valorpuro.replaceAllMapped(
+                      RegExp(r'^([0-9]{2})([0-9]{5})([0-9]{4})$'),
+                          (Match m) => '${m[1]} ${m[2]}-${m[3]} ',
+                    );
                     //Mudou mandou para a String
                   },
                   keyboardType: TextInputType.number,
@@ -137,6 +215,7 @@ class _cadastroEmpresaState extends State<cadastroEmpresa> {
                 child: TextFormField(
                   onChanged: (valor){
                     email = valor;
+                    telNumController.text = telNum;
                     //Mudou mandou para a String
                   },
                   keyboardType: TextInputType.emailAddress,
@@ -237,69 +316,84 @@ class _cadastroEmpresaState extends State<cadastroEmpresa> {
                                     );
                                   }else{
 
-                                    //to do
+                                     if(tags.isEmpty){
+                                       Fluttertoast.showToast(
+                                         msg: 'Adicione pelo menos um dia!',
+                                         toastLength: Toast.LENGTH_SHORT,
+                                         timeInSecForIosWeb: 1,
+                                         backgroundColor: Colors.black,
+                                         textColor: Colors.white,
+                                         fontSize: 20,
+                                       );
 
-                                    FirebaseApp app = await Firebase.initializeApp(
-                                        name: 'Secondary', options: Firebase.app().options);
-                                    try {
-                                      UserCredential userCredential = await FirebaseAuth.instanceFor(app: app)
-                                          .createUserWithEmailAndPassword(email: email, password: pass);
+                                     }else{
 
-                                      Fluttertoast.showToast(
-                                        msg: 'Cadastrando empresa...',
-                                        toastLength: Toast.LENGTH_SHORT,
-                                        timeInSecForIosWeb: 1,
-                                        backgroundColor: Colors.black,
-                                        textColor: Colors.white,
-                                        fontSize: 20,
-                                      );
-                                      FirebaseFirestore.instance.collection('empresa').doc(userCredential.user?.uid).set(
-                                          {
-                                            'nome': empresaName,
-                                            'galpaes': lista,
-                                            'NameResponsavel': respName,
-                                            'Telefone': telNum,
-                                            'email': email,
-                                            'tipoConta': 'empresa',
-                                            'estaativo': true,
-                                            'id': userCredential.user?.uid
-                                          }
-                                      );
-                                      FirebaseFirestore.instance.collection('Users').doc(userCredential.user?.uid).set(
-                                          {
-                                            'nome': empresaName,
-                                            'galpaes': lista,
-                                            'NameResponsavel': respName,
-                                            'Telefone': telNum,
-                                            'email': email,
-                                            'tipoConta': 'empresa',
-                                            'estaativo': true,
-                                            'id': userCredential.user?.uid,
-                                          }
-                                      ).then((value) {
-                                        Fluttertoast.showToast(
-                                          msg: 'Empresa cadastrada com sucesso!',
-                                          toastLength: Toast.LENGTH_SHORT,
-                                          timeInSecForIosWeb: 1,
-                                          backgroundColor: Colors.black,
-                                          textColor: Colors.white,
-                                          fontSize: 20,
-                                        );
-                                        Navigator.pop(context);
-                                      });
+                                       FirebaseApp app = await Firebase.initializeApp(
+                                           name: 'Secondary', options: Firebase.app().options);
+                                       try {
+                                         UserCredential userCredential = await FirebaseAuth.instanceFor(app: app)
+                                             .createUserWithEmailAndPassword(email: email, password: pass);
 
-                                    }
-                                    on FirebaseAuthException catch (e) {
-                                      Fluttertoast.showToast(
-                                        msg: e.message.toString(),
-                                        toastLength: Toast.LENGTH_SHORT,
-                                        timeInSecForIosWeb: 1,
-                                        backgroundColor: Colors.black,
-                                        textColor: Colors.white,
-                                        fontSize: 20,
-                                      );
-                                    }
-                                    await app.delete();
+                                         Fluttertoast.showToast(
+                                           msg: 'Cadastrando empresa...',
+                                           toastLength: Toast.LENGTH_SHORT,
+                                           timeInSecForIosWeb: 1,
+                                           backgroundColor: Colors.black,
+                                           textColor: Colors.white,
+                                           fontSize: 20,
+                                         );
+                                         FirebaseFirestore.instance.collection('empresa').doc(userCredential.user?.uid).set(
+                                             {
+                                               'nome': empresaName,
+                                               'galpaes': lista,
+                                               'NameResponsavel': respName,
+                                               'Telefone': telNum,
+                                               'email': email,
+                                               'tipoConta': 'empresa',
+                                               'estaativo': true,
+                                               'RelatorioDays': tags,
+                                               'id': userCredential.user?.uid
+                                             }
+                                         );
+                                         FirebaseFirestore.instance.collection('Users').doc(userCredential.user?.uid).set(
+                                             {
+                                               'nome': empresaName,
+                                               'galpaes': lista,
+                                               'NameResponsavel': respName,
+                                               'Telefone': telNum,
+                                               'email': email,
+                                               'tipoConta': 'empresa',
+                                               'estaativo': true,
+                                               'id': userCredential.user?.uid,
+                                               'RelatorioDays': tags,
+                                             }
+                                         ).then((value) {
+                                           Fluttertoast.showToast(
+                                             msg: 'Empresa cadastrada com sucesso!',
+                                             toastLength: Toast.LENGTH_SHORT,
+                                             timeInSecForIosWeb: 1,
+                                             backgroundColor: Colors.black,
+                                             textColor: Colors.white,
+                                             fontSize: 20,
+                                           );
+
+                                           tags.removeRange(0, tags.length);
+                                           Navigator.pop(context);
+                                         });
+
+                                       }
+                                       on FirebaseAuthException catch (e) {
+                                         Fluttertoast.showToast(
+                                           msg: e.message.toString(),
+                                           toastLength: Toast.LENGTH_SHORT,
+                                           timeInSecForIosWeb: 1,
+                                           backgroundColor: Colors.black,
+                                           textColor: Colors.white,
+                                           fontSize: 20,
+                                         );
+                                       }
+                                       await app.delete();
+                                     }
                                   }
                                 }
                               }
